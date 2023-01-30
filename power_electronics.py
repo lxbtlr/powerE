f2972574197234a9f1daf30270e5f2030ac9da97
Alex Butler <lxbtlr@pm.me>
2023-01-29 23:53:49 -0500

    Plecs model added

--- power_electronics.py
+++ power_electronics.py
@@ -2,8 +2,7 @@
 import matplotlib.pyplot as plt
 from numpy import arange
 import pandas as pd
-
-
+import math
 # parse log files and plot
 
 def plotter(names, data, information ):
@@ -14,7 +13,6 @@ def plotter(names, data, information ):
     figs = []
     for i, d in enumerate(data):
         # get time information from data
-
         t_inc   = d.loc[0,'Increment']
         t_start = d.loc[0,'Start']
         d = d.drop(0,axis = 0)
@@ -67,5 +65,62 @@ raw =  [pd.read_csv('./data/NewFile1.csv'),
 n_  =  ['4V', '6V', '8V', '9.69V']
 
 # call function 
-plotter(n_, raw, info)
+# plotter(n_, raw, info)
+
+
+def ccm_duty_cycle(s_period,a_period):
+    return a_period/s_period
+
+def voltage_converation_factor(v_out, v_in):
+    return v_out / v_in
+
+def ccm_ripple(V, L, s_period, v_con_fac):
+    
+    # inductor ripple current in CCM operation
+
+    i_out = .5 * (1-v_con_fac)*set_title*(V/L)
+
+    return i_out
+
+
+def ccm_peak_current(V, R, L, s_period, v_con_fac):
+    
+    # peak inductor current
+
+    ipk_out = (V/R) +  .5 * (1-v_con_fac) * s_period * (V/L)
+
+    return ipk_out
+
+def solve_k(R, L, s_period):
+    k = 2/s_period * L/R
+    return k
+
+def dcm_duty_cycle(K, v_con_fac):
+
+    # also written as sqrt(alpha)*M = sqrt(aplha)*DC_ccm
+
+    duty_cycle = math.sqrt(k) * math.sqrt(1/(1-v_con_fac)) * v_con_fac 
+
+    return duty_cycle
+
+def dcm_peak_current(K, v_con_fac):
+    ipk_out = math.sqrt(K)*math.sqrt(1-v_con_fac) * v_con_fac
+    return ipk_out
+
+def k_crit(v_con_fac):
+
+    return 1-v_con_fac
+
+def L_crit(R, s_period, v_con_fac)
+    # if L < L_crit, power converter will enter DCM
+
+    lcrit_out = (1-v_con_fac) * R * s_period
+
+    return lcrit_out
+
+def alpha(L, L_crit):
+
+    # also equal to K * (1 / 1-M)
+    return L/L_crit
+ 
 
